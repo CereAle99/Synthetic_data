c666a73c7850bf69120c3e67fc755f1cb45e3937
Ale <alessandro@ceresi.eu>
2023-01-12 23:52:31 +0100

    deleted the example for just one image

--- gaussian_filter.py
+++ gaussian_filter.py
@@ -12,19 +12,32 @@ import scipy.signal as signal
 
 x_train = x_train.reshape(x_train.shape[0], -1)
 y_train = y_train.reshape(y_train.shape[0], -1)
+x_test = x_test.reshape(x_test.shape[0], -1)
+y_test = y_test.reshape(y_test.shape[0], -1)
 
-x_train = pd.DataFrame(x_train)
-y_train = pd.DataFrame(y_train)
 
 # Normalizzazione dei dati
 x_train, x_test = x_train / 255.0, x_test / 255.0
+y_train, y_test = y_train / 255.0, y_test / 255.0
 
+
+'''
 # Visualizzare l'immagine numero i del dataframe df
 i, df = 6, x_train
-single_image = df.iloc[0, :].values.reshape(28,28)
+single_image = df[0, :].reshape(28,28)
 plt.imshow(single_image, cmap='gray')
 plt.show()
 
-#apply gaussian noise n times and show it at the end
-n = 500
-filter = signal.gaussian(size=(28,28), sigma=1)
+
+#apply gaussian filter n times and show it at the end
+n = 20
+convolved_image = single_image
+kernel = ndimage.gaussian_filter(np.ones([3,3]), sigma=1)
+for i in range(n):
+    convolved_image = signal.convolve2d(convolved_image, kernel, mode='same')
+
+plt.imshow(convolved_image, cmap='gray')
+plt.show()
+'''
+
+
