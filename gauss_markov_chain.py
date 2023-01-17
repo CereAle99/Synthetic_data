3875d9c7ef872c0f3740647f3945a8d3beef28e5
Ale <alessandro@ceresi.eu>
2023-01-17 13:31:45 +0100

    added function to get the markov matrix

--- gauss_markov_chain.py
+++ gauss_markov_chain.py
@@ -21,14 +21,11 @@ y_test = y_test.reshape(y_test.shape[0], -1)
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
-#creating a gaussian filter 
-kernel = ndimage.gaussian_filter(np.ones([3,3]), sigma=1)
-
 
 # Creazione della matrice di transizione con distribuzione gaussiana
 mean = 0
-std = 1
-transition_matrix = np.random.normal(mean, std, (28, 28))
+std = 0.01
+transition_matrix = np.random.normal(mean, std, (784, 784))
 
 '''
 #uniform transition matrix
@@ -40,6 +37,13 @@ transition_matrix = np.abs(transition_matrix)
 for i in range(transition_matrix.shape[0]):
     transition_matrix[i,:] = transition_matrix[i,:] / np.sum(transition_matrix[i,:])
 
+
+#function which get the markov matrix from the gaussian noise matrix
+gaussian_noise = np.random.normal(loc=0.0, scale=1, size=(28,28))
+mean, std = 0, 1
+def get_markov_gauss(matrix):
+    markov_matrix =  (1 / ( std * np.sqrt(2*np.pi))) * np.e ** (-((matrix - mean)**2) / (2*std**2))
+    return markov_matrix
 
 '''
 #check if something is wrong with visualization of matices
@@ -62,24 +66,24 @@ n = 1
 
 #appling the gaussian filter to x_train n times
 for i in range(x_train.shape[0]):
-    single_image = conv_x_train[i, :].reshape(28,28)
+    single_image = conv_x_train[i, :]
 
     for j in range(n):
-        new_state = transition_matrix @ single_image
+        new_state = np.dot(single_image, transition_matrix)
         single_image = new_state
     
-    conv_x_train[i, :] = single_image.reshape(1,784)
+    conv_x_train[i, :] = single_image
 
 
 #appling the gaussian filter to x_test n times
 for i in range(x_test.shape[0]):
-    single_image = conv_x_test[i, :].reshape(28,28)
+    single_image = conv_x_test[i, :]
 
     for j in range(n):
-        new_state = transition_matrix @ single_image
+        new_state = np.dot(single_image, transition_matrix)
         single_image = new_state
     
-    conv_x_test[i, :]= single_image.reshape(1,784) 
+    conv_x_test[i, :]= single_image
 
 
 #check if the algorithm worked
