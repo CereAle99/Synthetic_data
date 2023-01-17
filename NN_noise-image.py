f5ac2227693ecdd12cd2247d720713d792428cdd
Ale <alessandro@ceresi.eu>
2023-01-17 01:37:05 +0100

    changed the loss function in order to work with vectors lables

--- NN_noise-image.py
+++ NN_noise-image.py
@@ -10,10 +10,9 @@ import tensorflow as tf
 # Caricamento del dataset MNIST
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+#shaping 3D matrices as 2D, each row is an image
 x_train = x_train.reshape(x_train.shape[0], -1)
-y_train = y_train.reshape(y_train.shape[0], -1)
 x_test = x_test.reshape(x_test.shape[0], -1)
-y_test = y_test.reshape(y_test.shape[0], -1)
 
 # Normalizzazione dei dati
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -42,10 +41,18 @@ for i in range(x_test.shape[0]):
     
     noise_x_test[i, :] = single_image.reshape(1,784) 
 
+'''
+#reshaping the 2D matrices back as 3D, each row is a 28x28 bidimensional image (both x and noise_x)
+noise_x_train = x_train.reshape(noise_x_train.shape[0], 28, -1)
+x_train = x_train.reshape(x_train.shape[0], 28, -1)
+noise_x_test = x_test.reshape(noise_x_test.shape[0], 28, -1)
+x_test = x_test.reshape(x_test.shape[0], 28, -1)
+'''
+
 
 
 # Creiamo il layer di ingresso per i dati di input
-input_layer = layers.Input(shape=(28, 28))
+input_layer = layers.Input(shape=(784))
 
 # Creiamo la struttura della rete neurale, sono tre layer nascosti e un 
 # layer di output e il primo argomento sono i dnodi del layer
@@ -58,12 +65,30 @@ x = layers.Dense(784, activation='softmax')(x)
 model = models.Model(inputs=input_layer, outputs=x)
 
 # Compiliamo il modello
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])#I use the mean squared error because it allows to have vectors of shape (784) as lables
 
 # Eseguiamo il training del modello, il batch_size è il 
 # numero di esempi che usa ogni volta per aggiornare i pesi
-model.fit(x_train, y_train, epochs=5, batch_size=32)
+model.fit(noise_x_train, x_train, epochs=5, batch_size=32)
 
 # Valutiamo il modello utilizzando i dati di test
-test_loss, test_acc = model.evaluate(x_test, y_test)
+test_loss, test_acc = model.evaluate(noise_x_test, x_test)
 print('Test accuracy:', test_acc)
+
+
+#check if the algorithm worked with the j^th image if the sample
+j = 50
+try_image = x_train[j, :].reshape(28,28) + beta * np.random.normal(loc=0.0, scale=1, size=(28,28))
+    
+plt.imshow(show_image, cmap='gray')
+plt.show()
+
+
+
+
+
+
+
+
+
+
