2ad10d99051b721743b883336a99466e96f8444f
Ale <alessandro@ceresi.eu>
2023-02-28 01:27:18 +0100

    load and save weight implemented correctly

--- Vector-Quantized_Var_Autoencoders.py
+++ Vector-Quantized_Var_Autoencoders.py
@@ -219,11 +219,15 @@ data_variance = np.var(x_train / 255.0)
 
 vqvae_trainer = VQVAETrainer(data_variance, latent_dim=16, num_embeddings=128)
 vqvae_trainer.compile(optimizer=keras.optimizers.Adam())
-vqvae_trainer.fit(x_train_scaled, epochs=30, batch_size=128)
 
-#vqvae_trainer.load_weights('./checkpoints/my_checkpoint')
+#vqvae_trainer.fit(x_train_scaled, epochs=30, batch_size=128)
+
+# load the weights
+vqvae_trainer.load_weights('./checkpoints/my_checkpoint')
 
 
+# Save the weights
+vqvae_trainer.save_weights('./checkpoints/my_checkpoint')
 
 
 
