f1bfbae453cbc8a9b9e8a7cccd0b14d61726bab5
Ale <alessandro@ceresi.eu>
2023-03-12 19:05:54 +0100

    resized the flowers images from 180x180 to 160x160

--- VQVAE_model_flowers.py
+++ VQVAE_model_flowers.py
@@ -83,23 +83,23 @@ class VectorQuantizer(layers.Layer):
 
 
 def get_encoder(latent_dim=16):
-    encoder_inputs = keras.Input(shape=(180, 180, 3))
+    encoder_inputs = keras.Input(shape=(160, 160, 3))
     x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(
         encoder_inputs
     )
     x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)#eventualmente aggiungere un residual block
-    #x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2D(128, 3, activation="relu", strides=2, padding="same")(x)
     encoder_outputs = layers.Conv2D(latent_dim, 1, padding="same")(x)
     return keras.Model(encoder_inputs, encoder_outputs, name="encoder")
 
 
 def get_decoder(latent_dim=16):
     latent_inputs = keras.Input(shape=get_encoder(latent_dim).output.shape[1:])
-    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(
+    x = layers.Conv2DTranspose(128, 4, activation="relu", strides=2, padding="same")(
         latent_inputs
-    )
-    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-    #x = layers.Conv2DTranspose(32, 4, activation="relu", strides=2, padding="same")(x)
+    ) #dimesion of the filter 4x4 because in the decoding you jump a row each two and with 3x3 you evaluate only the zeros around your central pixel
+    x = layers.Conv2DTranspose(64, 4, activation="relu", strides=2, padding="same")(x) #dimesion of the filter
+    x = layers.Conv2DTranspose(32, 4, activation="relu", strides=2, padding="same")(x)
     decoder_outputs = layers.Conv2DTranspose(3, 3, padding="same")(x)
     return keras.Model(latent_inputs, decoder_outputs, name="decoder")
 
@@ -120,7 +120,7 @@ def get_vqvae(latent_dim=16, num_embeddings=64):
     vq_layer = VectorQuantizer(num_embeddings, latent_dim, name="vector_quantizer")
     encoder = get_encoder(latent_dim)
     decoder = get_decoder(latent_dim)
-    inputs = keras.Input(shape=(180, 180, 3))
+    inputs = keras.Input(shape=(160, 160, 3))
     encoder_outputs = encoder(inputs)
     quantized_latents = vq_layer(encoder_outputs)
     reconstructions = decoder(quantized_latents)
