15a3ca8950d3143fd4db4394a62d203ee1efae0f
Ale <alessandro@ceresi.eu>
2023-03-17 18:01:36 +0100

    new way of loading the model with tensorflow, don't know if it works

--- VQVAE_model_flowers.py
+++ VQVAE_model_flowers.py
@@ -5,6 +5,7 @@ from tensorflow import keras
 from keras import layers # keras for tensorflow.keras   
 import tensorflow_probability as tfp
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import pathlib
 import pandas as pd
 import os
@@ -178,7 +179,7 @@ class VQVAETrainer(keras.models.Model):
 
 
 
-
+'''
 # Training Dataset
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file(origin=dataset_url,
@@ -222,6 +223,52 @@ x_test_scaled = (x_test / 255.0) - 0.5
 
 data_variance = np.var(x_train / 255.0)
 
+'''
+
+
+# data
+dataset_name = "oxford_flowers102"
+dataset_repetitions = 5
+image_size = 160
+batch_size = 64
+
+def preprocess_image(data):
+    # center crop image
+    height = tf.shape(data["image"])[0]
+    width = tf.shape(data["image"])[1]
+    crop_size = tf.minimum(height, width)
+    image = tf.image.crop_to_bounding_box(
+        data["image"],
+        (height - crop_size) // 2,
+        (width - crop_size) // 2,
+        crop_size,
+        crop_size,
+    )
+
+    # resize and clip
+    # for image downsampling it is important to turn on antialiasing
+    image = tf.image.resize(image, size=[image_size, image_size], antialias=True)
+    return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+
+def prepare_dataset(split):
+    # the validation dataset is shuffled as well, because data order matters
+    # for the KID estimation
+    return (
+        tfds.load(dataset_name, split=split, shuffle_files=True)
+        .map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+        .cache()
+        .repeat(dataset_repetitions)
+        .shuffle(10 * batch_size)
+        .batch(batch_size, drop_remainder=True)
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+    )
+
+
+# load dataset
+x_train = prepare_dataset("train[:80%]+validation[:80%]+test[:80%]")
+x_test = prepare_dataset("train[80%:]+validation[80%:]+test[80%:]")
+data_variance = np.var(x_train / 255.0)
 
 
 
